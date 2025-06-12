41316077a2cf062964f86cdf9642fdfc3c8e677e
kimmunju-1130 <k22qqaa@gmail.com>
2025-06-12 18:58:06 +0900

    첫 내깃허브커밋

---
+++ src/main/java/com/example/practicehealthplatform/PracticeHealthPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicehealthplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeHealthPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeHealthPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicehealthplatform/PracticeHealthPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicehealthplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeHealthPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
